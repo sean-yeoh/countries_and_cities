4f644a2c019b83135b439feaaad106634a39473f
Sean Yeoh <sean.yeoh@hotmail.com>
2019-12-28 15:17:52 +0800

    add geocoder gem

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "shrine", "~> 3.0"
 gem 'country_select', '~> 4.0'
+gem 'geocoder'
--- app/controllers/select_country_and_city_controller.rb
+++ app/controllers/select_country_and_city_controller.rb
@@ -1,8 +1,15 @@
 class SelectCountryAndCityController < ApplicationController
   def index
     @countries = Country.all.order(name: :asc)
+
     if params[:country].present? && params[:city].present?
-      flash[:notice] = "Hello from #{ISO3166::Country[params[:country]]}, #{params[:city]}."
+      client_country_code = Geocoder.search(request.remote_ip).first.country
+
+      if client_country_code.present? && client_country_code != params[:country]
+        flash[:notice] = "Hey it looks like you are from #{ISO3166::Country[client_country_code]}."
+      else
+        flash[:notice] = "Hello from #{ISO3166::Country[params[:country]]}, #{params[:city]}."
+      end
     end
   end
 end
